7f5e88a737337f435e29305fc336bbf44bd52aae
Zechariah Walden <zech@zewadesign.com>
2015-06-26 13:55:16 -0500

    first run

--- puphpet/files/exec-once/run.sh
+++ puphpet/files/exec-once/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+su vagrant
 echo "Meteor Setup"
 curl https://install.meteor.com/ | /bin/sh
-mkdir -p '/apps'
-mkdir -p '/apps/codejet'
+APP_PATH='/var/www'
+cd ${APP_PATH}
 meteor create codejet
-APP_PATH='/srv/codejet'
+mv ${APP_PATH}'/codejet/.meteor' ${APP_PATH}'/'
+rm -rf ${APP_PATH}'/codejet'
 #rm -rf ${APP_PATH}/*
 LOCAL_DATA_PATH=${APP_PATH}'/.meteor/local/db'
 
@@ -13,22 +15,12 @@ LOCAL_DATA_PATH=${APP_PATH}'/.meteor/local/db'
 #meteor remove insecure
 #meteor add iron:router
 #meteor add appcache
-
-MONGO_STORAGE='/apps'
+sudo su
+MONGO_STORAGE='/mongodb'
 SRC_DATA=${MONGO_STORAGE}'/codejet'
 SRC_DATA_APP=${MONGO_STORAGE}'/codejet/db'
-CLIENT=${APP_PATH}'/client'
-PRIVATE=${APP_PATH}'/private'
-SERVER=${APP_PATH}'/server'
-LIB=${APP_PATH}'/lib'
-PUBLIC=${APP_PATH}'/public'
-ASSETS=${APP_PATH}'/client/assets'
-CSS=${ASSETS}'/css'
-SCSS=${ASSETS}'/scss'
-COLLECTIONS=${LIB}'/collections'
-VIEWS=${CLIENT}'/views'
 
-folders=($SRC_DATA $SRC_DATA_APP $CLIENT $SERVER $LIB $PUBLIC $PRIVATE $ASSETS $CSS $SCSS $COLLECTIONS $VIEWS)
+folders=($MONGO_STORAGE $SRC_DATA $SRC_DATA_APP $CLIENT $SERVER $LIB $PUBLIC $PRIVATE $ASSETS $CSS $SCSS $COLLECTIONS $VIEWS)
 for folder in "${folders[@]}"
  do
    mkdir ${folder}
@@ -36,5 +28,3 @@ for folder in "${folders[@]}"
 
 ln -s ${SRC_DATA_APP} ${LOCAL_DATA_PATH}
 chown vagrant:vagrant $SRC_DATA_APP
-touch ${LIB}'/router.js'
-touch ${APP_PATH}'/settings.json'
